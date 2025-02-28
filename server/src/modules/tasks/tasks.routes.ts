1879c90bac199a2c225c0cbcccace08ed3c6e8b4
sagun03 <sagunsaluja13@gmail.com>
2025-02-28 18:25:07 -0500

    updated task.route

--- server/src/modules/tasks/tasks.routes.ts
+++ server/src/modules/tasks/tasks.routes.ts
@@ -209,14 +209,14 @@ router.delete("/:id", async (req, res) => {
  *           type: array
  *           items:
  *             type: string
- *           example: ["boardId123", "boardId456", "boardId789"]
+ *           example: ["taskId123", "taskId456", "taskId789"]
  *     responses:
  *       200:
- *         description: List of boards found
+ *         description: List of tasks found
  *       400:
  *         description: Invalid request query
  *       404:
- *         description: Boards not found
+ *         description: Tasks not found
  *       500:
  *         description: Internal server error
  */
@@ -224,58 +224,4 @@ router.post("/fetchByIds", async (req, res) => {
   await taskController.getTasksByIds(req, res);
 });
 
-// Create a new task
-/**
- 
-@swagger
-/tasks:
-post:
-summary: Create a new task
-description: Adds a new task with relevant details
-requestBody:
-required: true
-content:
-application/json:
-schema:
-type: object
-properties:
-title:
-type: string
-example: "New Task"
-description:
-type: string
-example: "Task description here"
-priority:
-type: string
-enum: [high, medium, low]
-example: "high"
-dueDate:
-type: string
-format: date-time
-example: "2025-02-28T12:00:00Z"
-assignedTo:
-type: string
-example: "userId123"
-assignedBy:
-type: string
-example: "userId456"
-status:
-type: string
-enum: [To Do, In Progress, Done]
-example: "To Do"
-createdBy:
-type: string
-example: "adminId789"
-boardOriginalId:
-type: string
-example: "board123"
-responses:
-201:
-description: Task created successfully
-400:
-description: Invalid request body
-500:
-description: Internal server error*/
-router.post("/", async (req, res) => {
-    await taskController.createTask(req, res);
-  });
+export default router;
